974ff1b1c0a055c726b4e41e0fdb49582afbc4bb
Vasyll21 <vas.kyrychenko@gmail.com>
2022-11-25 09:20:48 +0200

    changed script.sh to correct adding softlink

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo useradd -m -p $(openssl passwd -1 -salt abc 123) adminuser
+sudo useradd -m -s /bin/bash -p $(openssl passwd -1 -salt abc 123) adminuser
 sudo usermod -a -G sudo adminuser
-sudo useradd -m poweruser
+sudo useradd -m -s /bin/bash poweruser
 sudo passwd -d poweruser
 echo 'poweruser All=(All:All) /usr/sbin/iptables' >> /etc/sudoers
 sudo usermod -a -G adminuser poweruser
-ln -s /home/poweruser/etc/mtab link
+ln -s /etc/mtab /home/poweruser/link
